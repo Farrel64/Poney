05d0ef794f72f4a72687507785ea155ed6ac820e
Thomas Morgana <thomas.morgana@y-nov.com>
2015-03-16 15:35:42 +0100

    Ajout des annonces parce que Bastient a ff alors je fais son boulot

--- wordpress/wp-content/themes/news-mix-light-child/functions.php
+++ wordpress/wp-content/themes/news-mix-light-child/functions.php
@@ -1,23 +1,41 @@
-<?php
+<?php 
 
-function admin_init(){ //initialisation des champs spécifiques
-add_meta_box("url_projet", "Url du projet", "url_projet", "projet", "normal", "low");  //il s'agit de notre champ personnalisé qui apelera la fonction url_projet()
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+  register_post_type( 'annonces',
+    array(
+      'labels' => array(
+        'name' => __( 'Petites Annonces' ),
+        'singular_name' => __( 'Petite Annonce' )
+      ),
+      'public' => true,
+      'supports' => array(       
+		'title',       
+		'editor',       
+		'thumbnail',
+		'custom-fields',
+		'comments',
+		),
+    )
+  );
 }
-function url_projet(){     //La fonction qui affiche notre champs personnalisé dans l'administration
-global $post;
-$custom = get_post_custom($post->ID); //fonction pour récupérer la valeur de notre champ
-$url_projet = $custom["url_projet"][0];
+	
+	
 ?>
-<input size="70" type="text" value="<?php echo $url_projet;?>" name="url_projet"/>
-<?php
-}
-function save_custom(){ //sauvegarde des champs spécifiques
-global $post;
-if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) { // fonction pour éviter  le vidage des champs personnalisés lors de la sauvegarde automatique
-return $postID;
-}
-update_post_meta($post->ID, "url_projet", $_POST["url_projet"]); //enregistrement dans la base de données
-}
 
-add_action("admin_init", "admin_init"); //function pour ajouter des champs personnalisés
-add_action('save_post', 'save_custom'); //function pour la sauvegarde de nos champs personnalisés
+<?php add_filter( 'pre_get_posts', 'my_get_posts' );
+
+function my_get_posts( $query ) {
+ if ( is_home() )
+ $query->set( 'post_type', array( 'annonces' ) );
+
+ return $query;
+}
+?>
+
+<?php 
+
+echo get_post_meta($post->ID, 'ICI_MON_TITRE_DE_CHAMPS', true) 
+
+?>
+
